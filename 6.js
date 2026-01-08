066ad51bee69ce0fa0be5ea6b6fd7a10d0082e96
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-01-08 13:22:19 +0600

    Solved

--- 6.js
+++ 6.js
@@ -5,3 +5,14 @@
 
 // Input: [1, 2, 3, 4]
 // Output: 10
+
+
+function sumArray(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4]));
