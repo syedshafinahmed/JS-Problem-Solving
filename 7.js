6c06bd12dd1c0ab6537ef42a9ba7250e009e1084
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-01-08 13:26:17 +0600

    Solved

--- 7.js
+++ 7.js
@@ -5,3 +5,16 @@
 
 // Input: [1, 2, 3, 4, 5, 6]
 // Output: [2, 4, 6]
+
+
+function findEvenNumbers(arr) {
+  let evenNumbers = [];
+  for(num of arr) {
+    if(num%2 === 0) {
+      evenNumbers.push(num);
+    }
+  }
+  return evenNumbers;  
+}
+
+console.log(findEvenNumbers([1,2,3,4,5,6]));
