5c29e89ef2f008642d2db06af488189558f64325
syedshafinahmed <shafinahmed.cse@gmail.com>
2026-01-08 13:48:00 +0600

    Solved

--- 8.js
+++ 8.js
@@ -5,3 +5,10 @@
 
 // Input: "hello world"
 // Output: "Hello World"
+
+
+function capitalizeLetters(str) {
+  return str.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+}
+
+console.log(capitalizeLetters("hello world"));
